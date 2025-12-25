188a6c0c1ed5ce1ebe69a24d05de0e0c08127d73
my282 <fm282@DESKTOP-FLQPLN7.localdomain>
2025-12-25 12:08:06 +0900

    solved a05

--- tessoku/a/a05.c
+++ tessoku/a/a05.c
@@ -2,6 +2,16 @@
 
 int main(void) {
   int n, k;
+  int a, b, c;
+  int answer = 0;
   scanf("%d%d", &n, &k);
+  for (a = 1; a <= n; a++) {
+    for (b = 1; b <= n; b++) {
+      c = k - a - b;
+      if (1 <= c && c <= n)
+        answer++;
+    }
+  }
+  printf("%d", answer);
   return 0;
 }
